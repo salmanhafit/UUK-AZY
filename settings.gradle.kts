c729629ab3ed6ceaf36a75cc829df9ce66763b70
salmanhafit <140377783+salmanhafit@users.noreply.github.com>
2024-02-29 15:41:41 +0700

    redme

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "calkulator_azi"
+include(":app")
+ 
